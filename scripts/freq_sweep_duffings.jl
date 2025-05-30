bac3f2cc54720e825b8432ccc28a7e7ed11f7fb3
awagemakers <awagemakers@localhost.localdomain>
2025-05-30 09:25:04 +0200

    save jld2

--- scripts/freq_sweep_duffings.jl
+++ scripts/freq_sweep_duffings.jl
@@ -9,7 +9,7 @@ include(srcdir("duffing_mapper.jl"))
 # N = 5; c = 0.1; k1 = 1.; k3 = 0.1; F = 0.4; kc = 0.05; ω = 1.2457
 N = 3; c = 0.1; k1 = 1.0; k3 = 0.5; F = 0.4; kc = 0.05; ω = 1.2457
 # prange = range(0.05, 0.5, length = 50)
-prange = range(1., 2., length = 20)
+prange = range(1., 2., length = 40)
 # prange = range(0.05, 0.5, step = 0.01)
 dps = duffing_parameters(N, c, k1, k3, F, kc, ω) 
 
@@ -23,9 +23,11 @@ ascm = AttractorSeedContinueMatch(mapper)
 pidx = :ω
 
 fractions_cont, attractors_cont = global_continuation(
-	ascm, prange, pidx, sampler; samples_per_parameter = 4000
+	ascm, prange, pidx, sampler; samples_per_parameter = 10000
 )
 
 fig = plot_basins_attractors_curves(
 	fractions_cont, attractors_cont, A -> minimum(A[:, 1]), prange,
 )
+
+@save "freq_sweep.jld2" fractions_cont attractors_cont fig
